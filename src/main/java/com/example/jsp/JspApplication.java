d1b87db9f98ab36c1a3201ed0beddf0860ba0f85
shaikjohnibegum-coder <shaikjohnibegum@gmail.com>
2026-05-15 23:20:33 -0400

    Hello World

---
+++ src/main/java/com/example/jsp/JspApplication.java
@@ -0,0 +1,15 @@
+package com.example.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspApplication.class, args);
+        System.out.println("Hello World");
+        System.out.println("Hello johni");
+    }
+
+}
